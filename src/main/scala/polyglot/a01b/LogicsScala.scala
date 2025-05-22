925441ed00b9d9eee5a82f0271336a231081a88a
RicciNicholas <riccinicho22@gmail.com>
2025-05-22 17:17:05 +0200

    corrected style in a01b ex3

--- src/main/scala/polyglot/a01b/LogicsScala.scala
+++ src/main/scala/polyglot/a01b/LogicsScala.scala
@@ -2,7 +2,6 @@ package polyglot.a01b
 
 import polyglot.OptionToOptional
 import util.Optionals.Optional as ScalaOptional
-import polyglot.a01b.Logics
 import util.Sequences.*
 import util.Sequences.Sequence.*
 import scala.util.Random
@@ -12,20 +11,22 @@ import scala.jdk.javaapi.OptionConverters
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 
 trait LogicsScala:
+  def size: Int
+  def mines: Int
   def hit(x: Int, y: Int): java.util.Optional[Integer]
-
   def won: Boolean
 
 object LogicsScala:
   def apply(size: Int, mines: Int): LogicsScala = LogicsImpl(size, mines)
 
-  private class LogicsImpl(private val _size: Int, private val _mines: Int) extends LogicsScala:
+  private class LogicsImpl(override val size: Int, override val mines: Int) extends LogicsScala:
 
     private var _minesPos = Sequence.Nil[(Int, Int)]()
     private var _countMines = 0
     private var _countClicks = 0
-    while (_countMines < _mines)
-      val pos = (Random.nextInt(_size), Random.nextInt(_size))
+
+    while _countMines < mines do
+      val pos = (Random.nextInt(size), Random.nextInt(size))
       if !_minesPos.contains(pos) then
         _minesPos = _minesPos.concat(Sequence.Cons(pos, Sequence.Nil()))
         _countMines = _countMines + 1
@@ -38,11 +39,11 @@ object LogicsScala:
       else
         _countClicks = _countClicks + 1
         var countNeighbors = 0
-        for 
+        for
           i <- x - 1 to x +1
           j <- y - 1 to y + 1
         yield countNeighbors = countNeighbors + (if _minesPos.contains((i, j)) then 1 else 0)
         OptionToOptional(ScalaOptional.Just(countNeighbors))
 
 
-    override def won: Boolean = _size * _size - _mines == _countClicks
+    override def won: Boolean = size * size - mines == _countClicks
